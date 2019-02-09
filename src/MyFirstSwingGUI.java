e09615795aed77a77e3c380f92c0052248e446a4
League <league@WTS9.attlocal.net>
2019-02-09 14:26:05 -0800

    Day 4
    JFrames, JPanels, JLabel

--- src/MyFirstSwingGUI.java
+++ src/MyFirstSwingGUI.java
@@ -5,6 +5,7 @@
 
 
 import java.io.IOException;
+import java.awt.Dimension;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -15,34 +16,36 @@ import javax.swing.JPanel;
 public class MyFirstSwingGUI {
 	public static void main(String[] args) {
 		//1. Create and initialize an object of the JFrame class
-
+		JFrame object = new JFrame();
 		//2. Set your JFrame object to be visible
-
+		object.setVisible(true);
 		//3. Run your program. Do you see your window? It's probably very small.
 		
 		//4. Set the default close operation to JFrame.EXIT_ON_CLOSE
-
+		object.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//5. Create and initialize an object of the JPanel class
-
+		JPanel object1 = new JPanel();
 		//6. Create and initialize an object of the JLabel class
-
+		JLabel object2 = new JLabel();
 		//7. Set the text of the JLabel to a lovely greeting.
-
-		//8. Add the JPanel object to the JFrame
-
+		object2.setText("Good morning!");
+		//8. Add	 the JPanel object to the JFrame
+		object.add(object1);
 		//9. Add the JLabel object to the JPanel
-
+		object1.add(object2);
 		//10. Pack your JFrame.
-
+		object.pack();
 		//11. Run your program again. Do you see your message.
+			
+		//12	. Use the loadImage method to set the icon of the JLabel object.  
+		object2.setIcon(loadImage());
 		
-		//12. Use the loadImage method to set the icon of the JLabel object.  
-
 		//13. Re-pack the JFrame object.
-		
+			object.pack();
 		//14. Run the program one more time. Do you see the image?
+		
 	}
-	
+		
 	public static ImageIcon loadImage(){
 		try {
 			return new ImageIcon(ImageIO.read(new MyFirstSwingGUI().getClass().getResourceAsStream("java.png")));
